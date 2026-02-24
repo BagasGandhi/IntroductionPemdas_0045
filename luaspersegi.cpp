83a19a2a46e9a9875aea1340d1f0ab7f22c6c34b
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-02-24 16:49:55 +0700

    interger main luas persegi

--- luaspersegi.cpp
+++ luaspersegi.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int main()
+
+{//start
+    float p,l, luas;
+
+    //langkah 2 : Masukkan panjang
+    cout << "Masukkan panjang : ";
+    cin >> p;
+    //langkah 3 : Masukkan lebar
+    cout << "Masukkan lebar : ";
+    cin >> l;
+    //langkah 4 : Hitung luas = p x l
+    luas = p * l;
+    //langkah 5 : 
+    cout << "Hasilnya = " << luas ;
+
+}//selesai
