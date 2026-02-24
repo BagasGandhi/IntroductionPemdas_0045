443ce578cf01bbfe943c76a5f5385532d798a21c
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-02-24 16:46:55 +0700

    interger main "perkenalan"

--- perkenalan.cpp
+++ perkenalan.cpp
@@ -2,3 +2,18 @@
 #include <iostream>
 using namespace std;
 
+//program utama
+int main() 
+{
+    //deklarasi variabel umur
+    int umur;
+    //memberi nilai dari variable
+    umur = 19;
+
+    //menampilkan isi dari string
+    cout << "bagas" << endl;
+    //menampilkan isi interger
+    cout << 88 << endl;
+    //menampilkan isi dari variabel
+    cout << umur << endl;
+}
